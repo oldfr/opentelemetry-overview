157242b2eb487752ba0f2d4fcfa260e5408693e9
oldfr <ankitha.codes@gmail.com>
2025-12-18 18:43:20 +0530

    propagator added

--- src/main/java/com/example/opentelemetry_overview/config/OpenTelemetryConfig.java
+++ src/main/java/com/example/opentelemetry_overview/config/OpenTelemetryConfig.java
@@ -1,10 +1,15 @@
 package com.example.opentelemetry_overview.config;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.extension.trace.propagation.B3Propagator;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.logs.SdkLoggerProvider;
 import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
@@ -58,11 +63,23 @@ public class OpenTelemetryConfig {
                 .setResource(resource)
                 .build();
 
+        ContextPropagators propagators = getContextPropagators();
+
         // ---------- Build SDK ----------
         return OpenTelemetrySdk.builder()
                 .setTracerProvider(tracerProvider)
                 .setMeterProvider(meterProvider)
                 .setLoggerProvider(loggerProvider)
+                .setPropagators(propagators)
                 .buildAndRegisterGlobal();
+    }
+
+    private ContextPropagators getContextPropagators() {
+        TextMapPropagator textMapPropagator = W3CTraceContextPropagator.getInstance(); // if w3c trace Propagation needed
+        W3CBaggagePropagator baggagePropagator = W3CBaggagePropagator.getInstance(); // if baggage Propagation needed
+
+        return ContextPropagators.create(
+                TextMapPropagator.composite(B3Propagator.injectingMultiHeaders())
+        );
     }
 }
